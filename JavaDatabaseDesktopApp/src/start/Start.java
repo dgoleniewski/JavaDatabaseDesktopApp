516d23e2051d70336eb327eee7bd2518878236d4
dgoleniewski <goleniewski.damian1@gmail.com>
2020-05-23 22:29:32 +0200

    NewNote from

--- JavaDatabaseDesktopApp/src/start/Start.java
+++ JavaDatabaseDesktopApp/src/start/Start.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javadatabasedesktopapp;
+package start;
 
 /**
  *
  * @author Goleniewski
  */
-public class JavaDatabaseDesktopApp {
+public class Start {
 
     /**
      * @param args the command line arguments
